831442be9cba3b1485f1dbbe07749d7b8b920b81
Ezequiel M Gioia <eze1981@Ezequiels-MacBook-Pro.local>
2019-01-14 20:32:48 -0500

    Displaying current date in clock plugin.

--- src/plugins/cuadro-plugin-clock/Clock.vue
+++ src/plugins/cuadro-plugin-clock/Clock.vue
@@ -1,9 +1,13 @@
 <template>
-  <analog-clock :minute="time.minutes" :tick="tick"></analog-clock>
+  <div>
+    <h1>{{date}}</h1>
+    <analog-clock :minute="time.minutes" :tick="tick"></analog-clock>
+  </div>
 </template>
 
 <script>
 import AnalogClock from '@/plugins/cuadro-plugin-clock/AnalogClock.vue'
+import Weather from '@/services/weather'
 
 // Taken from Nicolas Udy
 // https://vuejsexamples.com/a-text-based-and-analog-clock-with-vue-js/
@@ -12,18 +16,23 @@ export default {
   data() {
     return {
       tick: 0,
-      time: { hours: 0, minutes: 0, seconds: 0 }
+      time: { hours: 0, minutes: 0, seconds: 0 },
+      date: ""
     }
   },
   methods: {
     updateTime(time) {
-      // console.log(3333)
+
+      // get the time
       this.tick++
       this.time = {
         hours: time.getHours(),
         minutes: time.getMinutes(),
         seconds: time.getSeconds()
       }
+
+      // ge the date
+      this.date = time.toLocaleDateString('en-US', { weekday: 'long', year: 'numeric', month: 'long', day: 'numeric' })
 
       setTimeout(() => this.updateTime(new Date()), 1000 - new Date().getMilliseconds())
     }
